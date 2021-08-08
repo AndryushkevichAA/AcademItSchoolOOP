a16a26118236f2662d44cb2963181ea1da71a666
AndryushkevichAA <aligato123123@gmail.com>
2021-08-08 09:54:04 +0700

    Добавлена обработка исключения в getFileLinesList. Изменения в коде getNotRepeatingNumbersList.

--- ArrayListHome/src/ru/nsu/andryushkevich/array_list_home/ArrayListHome.java
+++ ArrayListHome/src/ru/nsu/andryushkevich/array_list_home/ArrayListHome.java
@@ -8,16 +8,18 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayListHome {
-    public static ArrayList<String> getFileLinesList(File file) throws FileNotFoundException {
-        try (Scanner scanner = new Scanner(new FileInputStream(file))) {
-            ArrayList<String> fileLines = new ArrayList<>();
+    public static ArrayList<String> getFileLinesList(File file) {
+        ArrayList<String> fileLines = new ArrayList<>();
 
-            while (scanner.hasNext()) {
+        try (Scanner scanner = new Scanner(new FileInputStream(file))) {
+            while (scanner.hasNextLine()) {
                 fileLines.add(scanner.nextLine());
             }
-
-            return fileLines;
+        } catch (FileNotFoundException e) {
+            System.out.println("Не удается найти указанный файл");
         }
+
+        return fileLines;
     }
 
     public static void removeEvenNumbers(ArrayList<Integer> numbers) {
@@ -31,33 +33,27 @@ public class ArrayListHome {
     }
 
     public static ArrayList<Integer> getNotRepeatingNumbersList(ArrayList<Integer> numbers) {
-        ArrayList<Integer> notRepeatingNumbers = new ArrayList<>(numbers);
+        ArrayList<Integer> notRepeatingNumbers = new ArrayList<>();
 
-        for (int i = 0; i < notRepeatingNumbers.size(); i++) {
-            for (int j = i + 1; j < notRepeatingNumbers.size(); j++) {
-                if (notRepeatingNumbers.get(i).equals(notRepeatingNumbers.get(j))) {
-                    notRepeatingNumbers.remove(j);
-
-                    j--;
-                }
+        for (Integer number : numbers) {
+            if (!notRepeatingNumbers.contains(number)) {
+                notRepeatingNumbers.add(number);
             }
         }
 
         return notRepeatingNumbers;
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         File file = new File("ArrayListHome/src/ru/nsu/andryushkevich/array_list_home/input.txt");
-
         System.out.println("Список строк файла: " + getFileLinesList(file));
 
         ArrayList<Integer> numbers1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8));
-        ArrayList<Integer> numbers2 = new ArrayList<>(Arrays.asList(2, 3, 3, 4, 5, 6, 5, 7));
-
         removeEvenNumbers(numbers1);
         System.out.println("Список нечетных чисел: " + numbers1);
 
+        ArrayList<Integer> numbers2 = new ArrayList<>(Arrays.asList(2, 3, 3, 4, 5, 6, 5, 7));
         ArrayList<Integer> notRepeatingNumbers = getNotRepeatingNumbersList(numbers2);
-        System.out.println("Список с неповторяющимися числами" + notRepeatingNumbers);
+        System.out.println("Список с неповторяющимися числами: " + notRepeatingNumbers);
     }
 }
