8c3a7750e266021bb90fe25adafdd0aad61cf2a4
AndryushkevichAA <aligato123123@gmail.com>
2021-08-22 13:05:28 +0700

    Задача "Лямбды"

---
+++ Lambda/src/ru/nsu/andryushkevich/lambda_main/Main.java
@@ -0,0 +1,53 @@
+package ru.nsu.andryushkevich.lambda_main;
+
+import ru.nsu.andryushkevich.person.Person;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Person> persons = Arrays.asList(
+                new Person("Максим", 28),
+                new Person("Андрей", 18),
+                new Person("Иван", 17),
+                new Person("Елена", 43),
+                new Person("Иван", 38),
+                new Person("Ольга", 13),
+                new Person("Артём", 10),
+                new Person("Артём", 28)
+        );
+
+        String uniqueNames = persons.stream()
+                .map(Person::getName)
+                .distinct()
+                .collect(Collectors.joining(", "));
+
+        System.out.println("Имена: " + uniqueNames);
+
+        List<Person> personsUnder18 = persons.stream()
+                .filter(person -> person.getAge() < 18)
+                .collect(Collectors.toList());
+
+        personsUnder18.stream()
+                .mapToInt(Person::getAge)
+                .average()
+                .ifPresent(averageAge -> System.out.println("Средний возраст людей до 18 лет: " + averageAge));
+
+        Map<String, Double> personsByName = persons.stream()
+                .collect(Collectors.groupingBy(Person::getName, Collectors.averagingDouble(Person::getAge)));
+
+        System.out.println("Имя: Средний возраст");
+        personsByName.forEach((name, averageAge) -> System.out.printf("%s: %s%n", name, averageAge));
+
+        List<Person> personsFrom20To45 = persons.stream()
+                .filter(person -> person.getAge() >= 20 && person.getAge() <= 45)
+                .sorted((person1, person2) -> person2.getAge() - person1.getAge())
+                .collect(Collectors.toList());
+
+        System.out.println("Имена людей от 20 до 45 лет в порядке убывания возраста: ");
+        personsFrom20To45.forEach(person -> System.out.println(person.getName()));
+    }
+}
